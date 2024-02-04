f3327babf3898f7e27f0f72ecc0fb73b37f51f9c
Arch-Gabriel <gtomazini@hotmail.com>
2024-02-04 18:33:29 -0300

    Exercicio novo

---
+++ exerc312.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// Um programa que resolve equação do segundo grau
+int main()
+{
+  // A, B e C são coeficientes dados de números reais.
+  double a, b, c;
+
+  std::cout << "Digite os valores de a, b e c [na ordem sequencial]: ";
+  std::cin >> a >> b >> c;
+
+  //ax² + bx + c = 0
+
+
+}
